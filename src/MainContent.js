d3b2c9660e1f00108fd490d92aad05834b96a610
Blin Kukaj <kblin164@gmail.com>
2022-11-07 03:13:00 +0100

    Created the main section (banner)

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./MainContent.css";
-import Background from './images/Background'
+import Background from "./images/background.jpeg";
 
 export default function MainContent() {
   return (
     <div>
       <div className="content">
-        <h1>Shop Now</h1>
-        <p>
-            New Arrivals
-        </p>
+        <h1>Shop Now*</h1>
+        <p>New Arrivals</p>
       </div>
+      <img src={Background} alt="the background" />
     </div>
   );
 }
