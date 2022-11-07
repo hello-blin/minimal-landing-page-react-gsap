c8cc66f10336590cfc11ac089c8271bce5e74d83
Blin Kukaj <kblin164@gmail.com>
2022-11-07 13:28:58 +0100

    Added more styling to the header, reduced the duration time of header elements to show up, added one more column to the header and used timeline.from to make the animation same as other header elements.

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,34 @@
 import React, { useRef, useEffect } from "react";
 import "./Header.css";
+import { RiShoppingBag2Line } from "react-icons/ri";
 
 function Header({ timeline }) {
   let logo = useRef(null);
   let menu_items = useRef(null);
+  let icon = useRef(null);
   useEffect(() => {
     timeline.from(logo, {
       delay: 0.5,
-      duration: 1,
+      duration: 0.4,
       opacity: 0,
       y: 100,
     });
-    timeline.from(menu_items, {
-      duration: 1,
+    timeline.from(icon, {
+      delay: 0.5,
+      duration: 0.4,
       opacity: 0,
       y: 100,
-    }, "-=.5");
+    });
+    timeline.from(
+      menu_items,
+      {
+        delay: 0.5,
+        duration: 0.4,
+        opacity: 0,
+        y: 100,
+      },
+      "-=.5"
+    );
   });
   return (
     <div>
@@ -29,6 +42,11 @@ function Header({ timeline }) {
             <li>About</li>
             <li>Contact</li>
           </ul>
+        </div>
+        <div className="cart" ref={(el) => (icon = el)}>
+          <RiShoppingBag2Line
+            size={25}
+           className="header-icon"></RiShoppingBag2Line>
         </div>
       </div>
     </div>
