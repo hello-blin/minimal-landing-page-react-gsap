5bd00dda33bd2f15c456156b524f454643ae2eff
Blin Kukaj <kblin164@gmail.com>
2022-11-07 01:54:46 +0100

    Created Header and placed it in the main entry App

---
+++ src/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MainContent.css";
+import Background from './images/Background'
+
+export default function MainContent() {
+  return (
+    <div>
+      <div className="content">
+        <h1>Shop Now</h1>
+        <p>
+            New Arrivals
+        </p>
+      </div>
+    </div>
+  );
+}
